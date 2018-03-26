dba5eea27ed3e195cea3611bf60360f8877a9d4f
dirtybits <dirtybitsofficial@gmail.com>
2018-03-26 05:44:54 -0500

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,17 @@
+#include <Blockchain.h>
+
+int main(){
+    Blockchain bChain = Blockchain();
+    
+    std::cout << "Mining block 1..." << std::endl;
+    bChain.AddBlock(Block(1, "Block 1 Data");
+    
+    std::cout << "Mining block 1..." << std::endl;
+    bChain.AddBlock(Block(1, "Block 1 Data");
+    
+    std::cout << "Mining block 1..." << std::endl;
+    bChain.AddBlock(Block(1, "Block 1 Data");
+    
+    return 0;
+}
+    
