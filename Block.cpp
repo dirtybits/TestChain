139b6d764135dcb7d85359f99daa4a0161000d28
dirtybits <dirtybitsofficial@gmail.com>
2018-03-26 06:11:06 -0500

    Update Block.cpp

--- Block.cpp
+++ Block.cpp
@@ -16,7 +16,7 @@ std::string Block::GetHash(){
 void Block::MineBlock(uint32_t nDifficulty){
     char cstr[nDifficulty + 1];
     for (uint32_t i = 0; i < nDifficulty; ++i){
-        cstr[i];
+        cstr[i] ='\0';
     }
     cstr[nDifficulty] = '\0';
     
@@ -35,5 +35,5 @@ inline std::string Block::_CalculateHash() const{
     std::stringstream ss;
     ss << _nIndex << _tTime << _sData << _nNonce << sPrevHash;
 
-    return sha256(ss.str);
+    return sha256(ss.str());
 }
