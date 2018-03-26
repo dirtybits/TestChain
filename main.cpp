20891d9799e1df38bf7372ccd710002f5dec54c9
dirtybits <dirtybitsofficial@gmail.com>
2018-03-26 05:51:11 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -4,13 +4,13 @@ int main(){
     Blockchain bChain = Blockchain();
     
     std::cout << "Mining block 1..." << std::endl;
-    bChain.AddBlock(Block(1, "Block 1 Data");
+    bChain.AddBlock(Block(1, "Block 1 Data"));
     
     std::cout << "Mining block 1..." << std::endl;
-    bChain.AddBlock(Block(1, "Block 1 Data");
+    bChain.AddBlock(Block(1, "Block 1 Data"));
     
     std::cout << "Mining block 1..." << std::endl;
-    bChain.AddBlock(Block(1, "Block 1 Data");
+    bChain.AddBlock(Block(1, "Block 1 Data"));
     
     return 0;
 }
